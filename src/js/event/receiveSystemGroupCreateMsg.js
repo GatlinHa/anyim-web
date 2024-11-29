cefcc4c189f1772acfdb8e0b1ca4f5be8a08d254
bob <312777916@qq.com>
2024-11-29 10:56:33 +0800

    messageData.updateSession和messageData.addMsgRecords都需要在session加载完之后才能执行，否则时机不对

--- src/js/event/receiveSystemGroupCreateMsg.js
+++ src/js/event/receiveSystemGroupCreateMsg.js
@@ -13,27 +13,26 @@ export const onReceiveSystemGroupCreateMsg = () => {
         sessionId: sessionId
       }).then((res) => {
         messageData.addSession(res.data.data)
+        messageData.updateSession({
+          sessionId: sessionId,
+          lastMsgId: msg.body.msgId,
+          lastMsgType: msg.header.msgType,
+          lastMsgContent: msg.body.content,
+          lastMsgAccount: msg.body.fromId,
+          lastMsgTime: now
+        })
+
+        messageData.addMsgRecords(sessionId, [
+          {
+            sessionId: sessionId,
+            msgId: msg.body.msgId,
+            fromId: msg.body.fromId,
+            msgType: msg.header.msgType,
+            content: msg.body.content,
+            msgTime: now
+          }
+        ])
       })
     }
-
-    messageData.updateSession({
-      sessionId: sessionId,
-      lastMsgId: msg.body.msgId,
-      lastMsgType: msg.header.msgType,
-      lastMsgContent: msg.body.content,
-      lastMsgAccount: msg.body.fromId,
-      lastMsgTime: now
-    })
-
-    messageData.addMsgRecords(sessionId, [
-      {
-        sessionId: sessionId,
-        msgId: msg.body.msgId,
-        fromId: msg.body.fromId,
-        msgType: msg.header.msgType,
-        content: msg.body.content,
-        msgTime: now
-      }
-    ])
   }
 }
