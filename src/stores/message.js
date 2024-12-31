3621c7ade9f00fd3f592d9c0da5b564eceb14c9f
bob <312777916@qq.com>
2024-12-31 09:58:45 +0800

    端侧不用做seq唯一性校验，由云侧保证，唯一的seq分配唯一的msgid

--- src/stores/message.js
+++ src/stores/message.js
@@ -54,11 +54,6 @@ export const messageStore = defineStore('anyim-message', () => {
    */
   const msgIdSortArray = ref({})
 
-  /**
-   * 用于消息唯一性校验，key是seq，value是msgId
-   */
-  const msgUniqueSeq = ref({})
-
   const addSession = (session) => {
     sessionList.value[session.sessionId] = session
   }
@@ -132,20 +127,12 @@ export const messageStore = defineStore('anyim-message', () => {
       if (!msgRecordsList.value[sessionId]) {
         msgRecordsList.value[sessionId] = {}
       }
-
-      // seq没有或者seq没有被缓存，才能add这条消息
-      if (!item.seq || !(item.seq in msgUniqueSeq.value)) {
-        msgRecordsList.value[sessionId][item.msgId] = item
-        if (item.seq) {
-          // seq不为空则添加至缓存
-          msgUniqueSeq.value[item.seq] = item.msgId
-        }
-      }
+      msgRecordsList.value[sessionId][item.msgId] = item
     })
     // 更新排序
-    msgIdSortArray.value[sessionId] = Object.keys(msgRecordsList.value[sessionId])
-      .map((key) => parseInt(key)) // Object.keys得到的是string数组
-      .sort((a, b) => a - b)
+    msgIdSortArray.value[sessionId] = Object.keys(msgRecordsList.value[sessionId]).sort(
+      (a, b) => a - b
+    )
   }
 
   /**
@@ -154,10 +141,9 @@ export const messageStore = defineStore('anyim-message', () => {
    * @param {*} msgId 消息id
    */
   const removeMsgRecord = (sessionId, msgId) => {
-    const msg = msgRecordsList.value[sessionId][msgId]
-    if (!msg) return
-    if (msg.seq in msgUniqueSeq.value) delete msgUniqueSeq.value[msg.seq]
-    if (msgId in msgRecordsList.value[sessionId]) delete msgRecordsList.value[sessionId][msgId]
+    if (msgId in msgRecordsList.value[sessionId]) {
+      delete msgRecordsList.value[sessionId][msgId]
+    }
   }
 
   const getMsg = (sessionId, msgId) => {
